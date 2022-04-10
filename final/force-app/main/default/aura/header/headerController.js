67346a108cfae36326b5c5c669412386828a9292
SlavaNoMercy <slava.nomercy@gmail.com>
2022-04-10 15:34:07 +0300

    create vfp to generate pdf

--- final/force-app/main/default/aura/header/headerController.js
+++ final/force-app/main/default/aura/header/headerController.js
@@ -1,5 +1,14 @@
 ({
-    myAction : function(component, event, helper) {
+    home : function(component, event, helper) {
+
+    },
+    dealers : function(component, event, helper) {
+
+    },
+    products : function(component, event, helper){
+
+    },
+    testDrive : function(component, event, helper) {
 
     }
 })
