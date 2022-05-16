67dcc5d9c5aaa28384c79ade6d9cfd6383ad76b7
SlavaNoMercy <slava.nomercy@gmail.com>
2022-05-16 18:10:38 +0300

    karusel

--- final/force-app/main/default/aura/productList/productListController.js
+++ final/force-app/main/default/aura/productList/productListController.js
@@ -4,12 +4,30 @@
     action.setCallback(component, function (response) {
       const state = response.getState();
       //console.log(response);
-      if (state === "SUCCESS") {
+      if (state === "SUCCESS") { 
         component.set("v.cars", response.getReturnValue());
+        console.log(component.get("v.cars"));
+        helper.showSlides(component,component.get("v.slideNumber"));//
       } else {
         console.log("Failed with state: " + state);
       }
     });
     $A.enqueueAction(action);
+  },
+  slidePlus: function(component,event,helper){
+    let i = parseInt(component.get("v.slideNumber")) + 1;
+    const slides = component.get("v.cars");
+    if (i > slides.length-1) {i = 0}
+    component.set("v.slideNumber",i);
+    console.log(i);
+    helper.showSlides(component,i);
+  },
+  slideMinus: function(component,event,helper){
+    let i = parseInt(component.get("v.slideNumber")) - 1;
+    const slides = component.get("v.cars");
+    if (i < 0 ) {i = slides.length-1}
+    component.set("v.slideNumber",i);
+    console.log(i);
+    helper.showSlides(component,i);
   }
 });
--- final/force-app/main/default/aura/productList/productListHelper.js
+++ final/force-app/main/default/aura/productList/productListHelper.js
@@ -1,5 +1,22 @@
 ({
-    helperMethod : function() {
-
+    showSlides : function(component,slideNumber) {
+        let i;
+        let slides = component.get("v.cars");
+        console.log(slideNumber,slides.length);
+        console.log(slides[slideNumber]["Id"]);
+        //if (slideNumber > slides.length) {slideNumber = 1}
+        console.log(slideNumber);
+        // if (slideNumber < 1) {slideNumber = slides.length-1}
+        console.log(slideNumber);
+        component.set("v.slideNumber",parseInt(slideNumber));
+        for (i = 0; i < slides.length; i++) {
+          let divId = document.getElementById(slides[i]["Id"]);
+          console.log(divId);
+          $A.util.removeClass(divId,"seeMe");
+          if(i==slideNumber){
+             $A.util.addClass(divId,"seeMe");
+            }          
+        }
+        console.log(slideNumber);  
     }
 })
