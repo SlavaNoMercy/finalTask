f97137ffd1b0038ff64927c45aa52c167286fbf9
SlavaNoMercy <slava.nomercy@gmail.com>
2022-05-19 18:02:27 +0300

    translate site using custom labels + add rsp forms

--- final/force-app/main/default/aura/footer/footerController.js
+++ final/force-app/main/default/aura/footer/footerController.js
@@ -1,8 +1,16 @@
 ({
-    topFunction:function(component,event,helper) {
-        console.log('works1'); 
-        helper.fireComponentEvent(component, event, "home");
-        window.scrollTo({top: 0,behavior: "smooth"});
-        console.log('works2');
+   topFunction:function(component,event,helper) {
+    console.log('works1'); 
+    helper.fireComponentEvent(component, event, "home");
+    window.scrollTo({top: 0,behavior: "smooth"});
+    console.log('works2');
+   },
+   topFunctionQ:function(component,event,helper) {
+    helper.fireComponentEvent(component, event, "question");
+    window.scrollTo({top: 0,behavior: "smooth"});
+   },
+   topFunctionTD:function(component,event,helper) {
+    helper.fireComponentEvent(component, event, "testDrive");
+    window.scrollTo({top: 0,behavior: "smooth"});
    }
 })
--- final/force-app/main/default/aura/header/headerController.js
+++ final/force-app/main/default/aura/header/headerController.js
@@ -1,6 +1,6 @@
 ({
   doInit: function(component,event,helper) {
-    console.log($A.get("$Locale.currency"))    
+    console.log($A.get("$Locale.currency")); 
   },
   home: function (component, event, helper) {
     helper.selectComponent(component,"home",component.get("v.currentPage"));
@@ -30,10 +30,23 @@
   selectCurrency: function(component,event,helper){
     console.log(event.target.value);
     helper.fireCurrChangeEvent(component,event.target.value);
-    console.log("event fired")
-  },
-  selectLanguage: function(component,event,helper){
-    console.log(event.target.value);
+
+    // const action = component.get("c.setNewSelectedCurrency");
+    
+    // console.log(currency);
+    // action.setParams({ selectedCurrency : currency });
+    // action.setCallback(component, function (response) {
+    //   const state = response.getState();
+    //   if (state === "SUCCESS") { 
+    //     component.set("v.cars", response.getReturnValue());
+    //     console.log(component.get("v.cars"));
+    //     helper.showSlides(component,component.get("v.slideNumber"));//
+    //   } else {
+    //     console.log("Failed with state: " + state);
+    //   }
+    // });
+    // $A.enqueueAction(action);
+    // console.log("event fired")
   },
   applyClasses: function(component,event,helper){
     let navItem = component.find("navigation");
